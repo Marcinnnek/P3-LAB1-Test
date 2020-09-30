89fc153982db26dd051e9beb076eaf069cc5f451
Marcin Zuziak <porda1995@gmail.com>
2020-09-30 18:14:57 +0200

    test

---
+++ P3 LAB1 app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P3_LAB1_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("GIT test");
+        }
+    }
+}
